29de35dccd85258074d9e1102e3514b0471b99d4
ANJANA1732 <anjusivakumar17302@gmail.com>
2025-03-18 14:27:42 +0530

    feat: multiple file upload and multiple share

--- src/main/java/com/example/rmss3/dto/ApiResponse.java
+++ src/main/java/com/example/rmss3/dto/ApiResponse.java
@@ -9,37 +9,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ApiResponse<T> {
     private int statusCode;
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public int getStatusCode() {
-        return statusCode;
-    }
-
-    public void setStatusCode(int statusCode) {
-        this.statusCode = statusCode;
-    }
-
-    public T getResponse() {
-        return response;
-    }
-
-    public void setResponse(T response) {
-        this.response = response;
-    }
-
     private String message;
     private T response;
-
-    public ApiResponse(int statusCode, String message, T response) {
-        this.statusCode = statusCode;
-        this.message = message;
-        this.response = response;
-    }
 }
